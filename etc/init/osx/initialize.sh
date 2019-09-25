58dc1823f6357b690130d8cf10b0553bd9a31e12
TAKAHASHI Osamu <zerosum@fountain.local>
2019-09-26 01:26:39 +0900

    add fzf & ghq

--- etc/init/osx/initialize.sh
+++ etc/init/osx/initialize.sh
@@ -2,6 +2,7 @@
 
 WORKING_DIR=$(cd $(dirname $0); pwd)
 
+### init Homebrew
 if [ -z $(which brew) ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -12,6 +13,7 @@ cat $WORKING_DIR/tapfiles | xargs brew tap
 cat $WORKING_DIR/caskfiles | xargs brew cask install
 cat $WORKING_DIR/brewfiles | xargs brew install
 
+### init zsh
 ZSH_BIN=/usr/local/bin/zsh
 if [ -z $(cat /etc/shells | grep $ZSH_BIN) ]; then
   echo $ZSH_BIN | sudo tee -a /etc/shells
