3a0b71a0f0e83e286c7b0657974edca0993cb254
zerosum <info@zerosum.dev>
2019-09-27 11:24:30 +0900

    modify .gitconfig

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -5,15 +5,22 @@ WORKING_DIR=$(cd $(dirname $0); pwd)
 
 cd $DOTPATH
 
+### deploy dotfiles
 for f in .??*
 do
-  [ "$f" = ".git" ] && continue
-  [ "$f" = ".DS_Store" ] && continue
+  [[ "$f" = ".git" ]] && continue
+  [[ "$f" = ".DS_Store" ]] && continue
+  [[ "$f" = ".gitconfig.local" ]] && continue
 
   unlink "$HOME"/"$f"
   ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
+ln -snfv "$DOTPATH"/_gitignore "$HOME"/.gitignore
+if [[ ! -e "$HOME/.gitconfig.local" ]]; then
+  ln -snfv "$DOTPATH"/.gitconfig.local "$HOME"/.gitconfig.local
+fi
 
+### deploy prompts
 ZPREZTODIR="${ZDOTDIR:-$HOME}/.zprezto"
 
 if [ -e $ZPREZTODIR ]; then
@@ -31,6 +38,7 @@ done
 echo 'zstyle :prezto:module:prompt theme powerlevel10k' >> ~/.zpreztorc
 echo '[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh' >> ~/.zshrc
 
+### deploy zsh scripts
 for zshfile in $(find ~/.zsh/ -name '*.zsh' -type f)
 do
   echo "[[ -f $zshfile ]] && source $zshfile" >> ~/.zshrc
