255b7783d91a59b0d65d4749e09bbb040d6b8c76
zerosum <info@zerosum.dev>
2022-04-03 00:58:48 +0900

    modify PATH for coursier

--- .zsh/.path.zsh
+++ .zsh/.path.zsh
@@ -2,6 +2,6 @@ typeset -U path PATH
 path=(
 	/opt/homebrew/bin(N-/)
 	/usr/local/bin(N-/)
-    $HOME/Library/ApplicationSupport/Coursier/bin(N-/)
+    $HOME/Library/Application\ Support/Coursier/bin(N-/)
 	$path
 )
