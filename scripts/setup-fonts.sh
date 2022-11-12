8b1022d48c017b15d3b9cc2cea4645565d5d663b
zerosum <345161+zerosum@users.noreply.github.com>
2022-11-12 23:00:16 +0900

    update HackGen

--- scripts/setup-fonts.sh
+++ scripts/setup-fonts.sh
@@ -6,14 +6,18 @@ source "$(dirname "$0")/common.sh"
 
 TMP_DIR_NAME=tmp$(date '+%Y%m%d%H%M%S')
 
+if [[ ! -d $XDG_DATA_HOME/fonts ]]; then
+    mkdir -p $XDG_DATA_HOME/fonts
+fi
+
 ### install HackGenNerd
 if [[ ! -d $XDG_DATA_HOME/fonts/HackGenNerd ]]; then
     mkdir -p $CUR_DIR/$TMP_DIR_NAME
     pushd $CUR_DIR/$TMP_DIR_NAME
-        hgn_version='v2.6.3'
-        curl -sSLO https://github.com/yuru7/HackGen/releases/download/$hgn_version/HackGenNerd_$hgn_version.zip
-        unzip HackGenNerd_$hgn_version.zip
-        sudo mv HackGenNerd_$hgn_version $XDG_DATA_HOME/fonts/HackGenNerd
+        hgn_version='v2.7.1'
+        curl -sSLO https://github.com/yuru7/HackGen/releases/download/$hgn_version/HackGen_NF_$hgn_version.zip
+        unzip HackGen_NF_$hgn_version.zip
+        sudo mv HackGen_NF_$hgn_version $XDG_DATA_HOME/fonts/HackGenNerd
         fc-cache
     popd
     rm -rf $CUR_DIR/$TMP_DIR_NAME
